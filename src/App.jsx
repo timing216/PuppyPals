fe1cbfce2afa8d291873ae9840c5754494f2e9ef
Utaro Hayashi <utaro.hayashi@gmail.com>
2023-06-12 00:56:22 -0500

    Added function  setFeatPupId(puppyId);

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
-  
+  function handleClick() {
+    // some logic here
+    setFeatPupId(puppyId);
+  }
+
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
     </div>
   );
 }
