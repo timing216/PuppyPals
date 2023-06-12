37aebd7d79f6460a5d7699263809fff92b2d9c63
Utaro Hayashi <utaro.hayashi@gmail.com>
2023-06-12 00:09:23 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { puppyList } from "./data.js";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  console.log("puppyList: ", puppyList);
   return (
-    <>
-      <div></div>
-    </>
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
